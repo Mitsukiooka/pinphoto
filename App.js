a5033e58cb950b09113568b6194c9bd9d8190025
Mitsukiooka <66833185+Mitsukiooka@users.noreply.github.com>
2020-12-01 13:12:12 +0900

    add react navigator

--- App.js
+++ App.js
@@ -1,25 +1,21 @@
 // import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+// import React from 'react';
+// import { StyleSheet, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+// import { StackNavigator } from 'react-navigation';
 
-import Appbar from './src/components/Appbar';
-import SignupScreen from './src/screens/SignupScreen';
+// import Appbar from './src/components/Appbar';
+// import SignupScreen from './src/screens/SignupScreen';
+import PinPhotoScreen from './src/screens/PinPhotoScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Appbar />
-      <SignupScreen />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ffddf6',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: 78,
-  },
+const App = createStackNavigator({
+  Home: { screen: PinPhotoScreen },
+}, {
+  defaultNavigationOptions: {
+    // ... オプション
+  }
 });
+
+export default createAppContainer(App);
+
